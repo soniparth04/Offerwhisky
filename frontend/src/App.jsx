101a20465719c80a2e6600954d145959ca55e053
Parth Soni <parthsoni4039@gmail.com>
2025-03-13 13:10:30 +0530

    Add back button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         <Route path="/shop-owner-dashboard" element={<OwnerDashboard />} />
         <Route path="/shop-owner/view-users" element={ <ViewUser/>}/>
         <Route path="/shop-owner/view-offers" element={ <ViewOffers />} />
-        <Route path="/add-offer" element={<AddOffer />} />
+        <Route path="/shop-owner/add-offer" element={<AddOffer />} />
         <Route path="/edit-offer/:offerId" element={<EditOffer />} />
         <Route path="/owner/view-redeemed/:userId" element={<ViewClaim />} />
         <Route path = "/shop-owner-registration" element={<OwnerRegistration/>}/>
