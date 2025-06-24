6945bc93cce58b35b5baced06db7ed8d4657727c
Parth Soni <parthsoni4039@gmail.com>
2025-06-24 16:16:39 +0530

    update

--- frontend/src/components/owner/DashComp/SponsoredAds.jsx
+++ frontend/src/components/owner/DashComp/SponsoredAds.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { ArrowLeft } from 'lucide-react';
+import { ArrowLeft, Plus } from 'lucide-react';
 import BottomNavigation from '../Navbar/BottomNav';
 
 const SponsoredAds = () => {
