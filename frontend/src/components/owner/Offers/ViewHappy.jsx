d21aa62485df9d8ee7353e1ae44cf6ba4a497570
Parth Soni <parthsoni4039@gmail.com>
2025-06-26 13:37:45 +0530

    update happy hour frontend

--- frontend/src/components/owner/Offers/ViewHappy.jsx
+++ frontend/src/components/owner/Offers/ViewHappy.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import OfferTimer from './OfferTimer';
-import { Pencil, Trash2 } from 'lucide-react';
+import { Eye, Zap } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 const GetAllHappyHours = () => {
