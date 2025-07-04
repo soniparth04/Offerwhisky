ceae0ba72d6d39b38a4e5ace885d57ec30362ba7
Parth Soni <parthsoni4039@gmail.com>
2025-07-04 13:07:33 +0530

    solve

--- frontend/src/components/owner/Offers/ViewSpotlight.jsx
+++ frontend/src/components/owner/Offers/ViewSpotlight.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate , useSearchParams} from "react-router-dom";
 import { ChevronRight, Tag, Eye, Zap } from 'lucide-react';
 
 const ViewSpotlightOffer = () => {
